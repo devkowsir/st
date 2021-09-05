1577e8ac12e98b692c2591a36b0420feee46f3e2
kowsirahmed <mdkowsir369@gmail.com>
2021-09-05 23:59:08 +0600

    font2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono Nerd Font Mono:antialias=true:pixelsize=12:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
+};
+
 static int borderpx = 2;
 
 /*
